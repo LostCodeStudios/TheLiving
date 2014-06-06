9af74aec3a440e2b804eea9f8e44adeac6a3d369
Nathaniel Nelson <nelson.nleroy@gmail.com>
2014-06-06 14:02:43 -0600

    Generated the Libgdx projects for the game.

---
+++ TheLiving/android/src/com/lostcodestudios/theliving/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.lostcodestudios.theliving.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.lostcodestudios.theliving.TheLiving;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TheLiving(), config);
+	}
+}
---
+++ TheLiving/desktop/src/com/lostcodestudios/theliving/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lostcodestudios.theliving.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lostcodestudios.theliving.TheLiving;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TheLiving(), config);
+	}
+}
